33c74cb68f0507741dd8cc8774d43faf23b350b7
abiryusuf <yusuf_ctg@yahoo.com>
2021-10-14 19:53:25 -0400

    screenshots

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from pageObjects.LoginPage import LoginPage
 
 
@@ -12,16 +13,22 @@ class Test_01_Login:
         self.driver = setup
         self.driver.get(self.baseURL)
         act_title = self.driver.title
-        self.driver.close()
+        # self.driver.close()
 
         if act_title == "Your store. Login":
             assert True
+            self.driver.close()
         else:
+            self.driver.save_screenshot("./Screenshots/" + "test_homePageTitle.png")
             assert False
+            self.driver.close()
 
     def test_login(self, setup):
+        # self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        # or
         self.driver = setup
         self.driver.get(self.baseURL)
+        # create an object for POM
         self.lp = LoginPage(self.driver)
         self.lp.setUserName(self.username)
         self.lp.setPassword(self.password)
@@ -31,5 +38,8 @@ class Test_01_Login:
 
         if act_title == "Dashboard / nopCommerce administration":
             assert True
+            self.driver.close()
         else:
+            self.driver.save_screenshot("./Screenshots/" + "test_login.png")
             assert False
+            self.driver.close()
