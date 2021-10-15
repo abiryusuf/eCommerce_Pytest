aac2be659d3b3570d0ba4ae60c5848b8ee862f05
abiryusuf <yusuf_ctg@yahoo.com>
2021-10-15 18:10:38 -0400

    cross browsers

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+def setup(browser):
+    if browser == 'chrome':
+        print("lunching chrome")
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(GeckoDriverManager().install())
+    else:
+        driver = webdriver.Ie()
     return driver
+
+def pytest_addoption(parser):    # This will get the value from CLI /hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):  # This will return the Browser value to setup method
+    return request.config.getoption("--browser")
