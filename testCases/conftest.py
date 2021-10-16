8930dcc81a38429cdcf017ed15a7c84c2db9778b
abiryusuf <yusuf_ctg@yahoo.com>
2021-10-15 20:49:39 -0400

    DDT

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -20,3 +20,17 @@ def pytest_addoption(parser):    # This will get the value from CLI /hooks
 @pytest.fixture()
 def browser(request):  # This will return the Browser value to setup method
     return request.config.getoption("--browser")
+
+# pytest HTML Report
+# command pytest -s -v -n=2 --html=Reports/reports.html testCase/test_login.py --browser chrome
+# It is hook for Adding Environment info to HTML Report
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'eCommerce'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['Tester'] = 'Abir'
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
