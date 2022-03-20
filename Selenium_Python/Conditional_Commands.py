298d0624c82d3ac5adfcecb47b3d5db3e5cbf5e6
abiryusuf <yusuf_ctg@yahoo.com>
2022-03-19 21:36:07 -0400

    selenium

--- Selenium_Python/Conditional_Commands.py
+++ Selenium_Python/Conditional_Commands.py
@@ -1,24 +1,17 @@
-import time
+# is_Displayed
+# is_enabled
+# is_selected
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-
 driver = webdriver.Chrome(service=Service(executable_path="/Users/abiryusuf/Documents/eCommerce/Drivers/chromedriver2"))
-driver.get("https://www.google.com")
-time.sleep(5)
-print(driver.title)
-
 driver.get("https://admin-demo.nopcommerce.com/")
-print(driver.title)
+ele = driver.find_element(By.XPATH, "//*[text() ='Welcome, please sign in!']")
+print(ele.is_displayed())
+print(ele.is_enabled())
 time.sleep(5)
-driver.back()
-time.sleep(5)
-driver.forward()
-print(driver.title)
-# driver.find_element(By.XPATH, "//*[@type ='submit']").click()
+driver.find_element(By.XPATH, "//*[@type ='submit']").click()
 
-print(driver.current_url)
-#driver.quit()
