8e66df2e15785639411fd2ef4c145a75905b7ba4
abiryusuf <yusuf_ctg@yahoo.com>
2022-03-22 20:41:50 -0400

    wait time

--- Selenium_Python/Implicit_Wait.py
+++ Selenium_Python/Implicit_Wait.py
@@ -1,14 +1,16 @@
-# is_Displayed
-# is_enabled
-# is_selected
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 import time
+
 driver = webdriver.Chrome(service=Service(executable_path="/Users/abiryusuf/Documents/eCommerce/Drivers/chromedriver2"))
 driver.get("https://admin-demo.nopcommerce.com/")
+driver.implicitly_wait(5)  # tell the webdriver wait for each elements
+# wait
+
+assert "Your store. Login" in driver.title
+
 ele = driver.find_element(By.XPATH, "//*[text() ='Welcome, please sign in!']")
 if ele.is_displayed():
     assert True
@@ -16,6 +18,3 @@ else:
     assert False
 time.sleep(5)
 driver.find_element(By.XPATH, "//*[@type ='submit']").click()
-
-#print(ele.is_selected())
-
