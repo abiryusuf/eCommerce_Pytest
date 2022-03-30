f3972897a32e2e7c0bc94c416c34601017925f8d
abiryusuf <yusuf_ctg@yahoo.com>
2022-03-29 20:58:58 -0400

    update the links

--- Selenium_Python/Links.py
+++ Selenium_Python/Links.py
@@ -7,3 +7,7 @@ driver.get("https://www.google.com/")
 
 links = driver.find_elements(By.TAG_NAME, "a")
 print(len(links))
+for link in links:
+    print("Name of the link", link.text)
+driver.find_element(By.LINK_TEXT, "Gmail").click()
+driver.find_element(By.PARTIAL_LINK_TEXT, "Gma").click()
